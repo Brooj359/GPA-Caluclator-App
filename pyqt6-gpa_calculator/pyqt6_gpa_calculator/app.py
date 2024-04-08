4f1dcf748244c4ab5d393ee6c8cd41c102c837a8
Brooks_Jason <brooj359@hsd.k12.or.us>
2024-04-08 15:07:19 -0700

    Started, added all 8 grades and working calculate button, main focus should be on styling and moving things around

--- pyqt6-gpa_calculator/pyqt6_gpa_calculator/app.py
+++ pyqt6-gpa_calculator/pyqt6_gpa_calculator/app.py
@@ -20,23 +20,54 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Widgets App")
 
-        layout = QVBoxLayout()
-        widgets = [
-            QCheckBox,
-            QLabel,
-            QPushButton,
-            QSpinBox,
-        ]
+        self.UiComponents()
+    
+    def UiComponents(self):
 
-        for w in widgets:
-            layout.addWidget(w())
+            self.layout = QVBoxLayout()
 
-        widget = QWidget()
-        widget.setLayout(layout)
+            self.widget = QWidget()
+            self.widget.setLayout(self.layout)
 
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
-        self.setCentralWidget(widget)
+            self.title_label = QLabel("GPA Calculator")
+            self.layout.addWidget(self.title_label)
+
+
+            self.gpa1_spinbox = QSpinBox()
+            self.gpa2_spinbox = QSpinBox()
+            self.gpa3_spinbox = QSpinBox()
+            self.gpa4_spinbox = QSpinBox()
+            self.gpa5_spinbox = QSpinBox()
+            self.gpa6_spinbox = QSpinBox()
+            self.gpa7_spinbox = QSpinBox()
+            self.gpa8_spinbox = QSpinBox()
+            
+            self.layout.addWidget(self.gpa1_spinbox)
+            self.layout.addWidget(self.gpa2_spinbox)
+            self.layout.addWidget(self.gpa3_spinbox)
+            self.layout.addWidget(self.gpa4_spinbox)
+            self.layout.addWidget(self.gpa5_spinbox)
+            self.layout.addWidget(self.gpa6_spinbox)
+            self.layout.addWidget(self.gpa7_spinbox)
+            self.layout.addWidget(self.gpa8_spinbox)
+
+            self.gpa_label = QLabel("GPA")
+            self.layout.addWidget(self.gpa_label)
+
+            # Set the central widget of the Window. Widget will expand
+            # to take up all the space in the window by default.
+            self.setCentralWidget(self.widget)
+            self.calculate_btn = QPushButton(text = "Calculate")
+            self.layout.addWidget(self.calculate_btn)
+            self.calculate_btn.pressed.connect(self.calculate_gpa)
+
+
+    def calculate_gpa(self):
+                
+        self.gpa_label.setText(str((int(self.gpa1_spinbox.value()) + int(self.gpa2_spinbox.value()) + int(self.gpa3_spinbox.value()) + int(self.gpa4_spinbox.value()) + int(self.gpa5_spinbox.value()) + int(self.gpa6_spinbox.value()) + int(self.gpa7_spinbox.value()) + int(self.gpa8_spinbox.value()))/8)) 
+
+
+
 
 
 app = QApplication(sys.argv)
@@ -44,3 +75,4 @@ window = MainWindow()
 window.show()
 
 app.exec()
+
